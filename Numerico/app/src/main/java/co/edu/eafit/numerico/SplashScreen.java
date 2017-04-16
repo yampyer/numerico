f8c75081b6191bf1bac18b9639a28de0b7afa7a0
Jean Pierre Gomez <jean.pierre@talosdigital.com>
2017-04-16 01:12:12 -0500

    Adding basic structure to project

---
+++ Numerico/app/src/main/java/co/edu/eafit/numerico/SplashScreen.java
@@ -0,0 +1,17 @@
+package co.edu.eafit.numerico;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
